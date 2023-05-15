726c694118bd2feaa04686e3fdc9d202e532f4c3
WinBSOD <91769343+WinBSOD@users.noreply.github.com>
2023-05-15 13:50:00 +0800

    Add files via upload

---
+++ 1.bat
@@ -0,0 +1,11 @@
+echo 请将此文件和文件夹内空的AppraiserRes.dll文件放在系统盘根目录
+
+cd %SYSTEMDRIVE%\
+mkdir 动态链接库备份
+copy %SYSTEMDRIVE%\$WINDOWS.~BT\Sources\AppraiserRes.dll %SYSTEMDRIVE%\动态链接库备份
+
+del /f /s /q %SYSTEMDRIVE%\$WINDOWS.~BT\Sources\AppraiserRes.dll
+
+copy  %SYSTEMDRIVE%\AppraiserRes.dll %SYSTEMDRIVE%\$WINDOWS.~BT\Sources\
+
+echo 若要调试程序，请在最后一行加上@pause
---
+++ 启动.bat
@@ -0,0 +1,4 @@
+﻿# 自动以管理员身份运行
+
+%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("1.bat","/c %~s0 ::","","runas",1)(window.close)&&exit cd /d "%~dp0"
+

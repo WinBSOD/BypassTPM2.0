4af4c9d9b026b752f91f44b8d18766bfa76aea27
WinBSOD <WinBSOD-2333@outlook.com>
2023-06-06 19:11:47 +0800

    Delete 启动程序组.bat

--- 启动程序组.bat
+++
@@ -1,5 +0,0 @@
-echo 正在获取管理员权限
-
-%1 mshta vbscript:CreateObject("Shell.Application").ShellExecute("%SYSTEMDRIVE%\1.bat","/c %~s0 ::","","runas",1)(window.close)&&exit cd /d "%~dp0"
-
-echo 请将此文件放在系统启动文件夹
